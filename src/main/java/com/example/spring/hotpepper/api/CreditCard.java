7462c6fdc5db7ccaace2b95d6ed794b186cf3469
asahinadev <dev.account.01@mail.mirageworld.jp>
2019-05-25 11:02:18 +0900

    クレジットカードマスタAPI
    http://webservice.recruit.co.jp/hotpepper/credit_card/v1/

---
+++ src/main/java/com/example/spring/hotpepper/api/CreditCard.java
@@ -0,0 +1,24 @@
+package com.example.spring.hotpepper.api;
+
+import java.net.URI;
+
+import org.springframework.stereotype.Service;
+
+import com.example.spring.hotpepper.parts.ApiBase;
+import com.example.spring.hotpepper.request.v1.CreditCardRequest;
+import com.example.spring.hotpepper.responce.v1.CreditCardResponse;
+
+@Service
+public class CreditCard
+		extends ApiBase<CreditCardRequest, CreditCardResponse> {
+
+	@Override
+	protected Class<CreditCardResponse> type() {
+		return CreditCardResponse.class;
+	}
+
+	@Override
+	protected URI uri() {
+		return config.getHotpepper().getCreditCard();
+	}
+}
