52264c99e77da8257a5caceec141018543a2dc8a
asahinadev <dev.account.01@mail.mirageworld.jp>
2019-05-25 15:30:15 +0900

    ジャンルマスタ API
    http://webservice.recruit.co.jp/manabi/genre/v2/

---
+++ src/main/java/com/example/spring/manabi/request/v2/GenreRequest.java
@@ -0,0 +1,29 @@
+package com.example.spring.manabi.request.v2;
+
+import java.util.List;
+
+import org.springframework.util.MultiValueMap;
+
+import com.example.spring.parts.RequestType;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@SuppressWarnings("serial")
+public class GenreRequest extends RequestType {
+
+	@JsonProperty("genre")
+	List<String> genres;
+
+	public MultiValueMap<String, String> convert() {
+		MultiValueMap<String, String> map = super.convert();
+
+		add(map, "genre", genres);
+
+		return map;
+	}
+
+}
---
+++ src/main/java/com/example/spring/manabi/response/v2/GenreResponse.java
@@ -0,0 +1,25 @@
+package com.example.spring.manabi.response.v2;
+
+import java.util.List;
+
+import com.example.spring.manabi.response.v2.sub.Genre;
+import com.example.spring.parts.ResponseType;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@SuppressWarnings("serial")
+public class GenreResponse extends ResponseType<GenreResponse.Results> {
+
+	@Getter
+	@Setter
+	public static class Results extends ResponseType.Results {
+
+		@JsonProperty("genre")
+		List<Genre> genres;
+
+	}
+}
