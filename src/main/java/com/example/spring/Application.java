b545b55801b4424f5e66a4eb113a17286504dd8b
asahinadev <dev.account.01@mail.mirageworld.jp>
2019-05-25 15:29:51 +0900

    applicationレベルでの命名規則の重複対応

--- src/main/java/com/example/spring/Application.java
+++ src/main/java/com/example/spring/Application.java
@@ -7,11 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @SpringBootApplication
+@ComponentScan(nameGenerator = AppNameGenerator.class)
 public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
