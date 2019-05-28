fa4483f5c39736cbbb2738800d4ad0815d0075d0
asahinadev <dev.account.01@mail.mirageworld.jp>
2019-05-28 15:21:08 +0900

    ボディタイプマスタAPI
    http://webservice.recruit.co.jp/carsensor/body/v1/

---
+++ src/main/java/com/example/spring/webapi/carsensor/v1/api/Body.java
@@ -0,0 +1,24 @@
+package com.example.spring.webapi.carsensor.v1.api;
+
+import java.net.URI;
+
+import org.springframework.stereotype.Service;
+
+import com.example.spring.common.ApiBase;
+import com.example.spring.webapi.carsensor.v1.request.BodyRequest;
+import com.example.spring.webapi.carsensor.v1.responce.BodyResponse;
+
+@Service
+public class Body
+		extends ApiBase<BodyRequest, BodyResponse> {
+
+	@Override
+	protected Class<BodyResponse> type() {
+		return BodyResponse.class;
+	}
+
+	@Override
+	protected URI uri() {
+		return config.getCarcensor().getBody();
+	}
+}
