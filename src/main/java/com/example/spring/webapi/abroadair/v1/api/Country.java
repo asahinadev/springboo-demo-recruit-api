4f08a4751fe5f3970f8802376d18c313e1164e79
asahinadev <dev.account.01@mail.mirageworld.jp>
2019-05-28 18:18:37 +0900

    国マスタAPI（仮）
    http://webservice.recruit.co.jp/ab-road-air/country/v1//

---
+++ src/main/java/com/example/spring/webapi/abroadair/v1/api/Country.java
@@ -0,0 +1,24 @@
+package com.example.spring.webapi.abroadair.v1.api;
+
+import java.net.URI;
+
+import org.springframework.stereotype.Service;
+
+import com.example.spring.common.ApiBase;
+import com.example.spring.webapi.abroadair.v1.request.CountryRequest;
+import com.example.spring.webapi.abroadair.v1.responce.CountryResponse;
+
+@Service
+public class Country
+		extends ApiBase<CountryRequest, CountryResponse> {
+
+	@Override
+	protected Class<CountryResponse> type() {
+		return CountryResponse.class;
+	}
+
+	@Override
+	protected URI uri() {
+		return config.getAbroadair().getCountry();
+	}
+}
