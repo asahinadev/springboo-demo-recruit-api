2aca99560c541885c5c179ffa34c31648d21370c
asahinadev <dev.account.01@mail.mirageworld.jp>
2019-05-29 15:45:32 +0900

    こだわりマスタAPI
    http://webservice.recruit.co.jp/ab-road/kodawari/v1/

---
+++ src/main/java/com/example/spring/webapi/abroad/v1/api/Kodawari.java
@@ -0,0 +1,27 @@
+package com.example.spring.webapi.abroad.v1.api;
+
+import java.net.URI;
+
+import org.springframework.stereotype.Service;
+
+import com.example.spring.common.rest.ApiBase;
+import com.example.spring.webapi.abroad.v1.request.KodawariRequest;
+import com.example.spring.webapi.abroad.v1.responce.KodawariResponse;
+
+@Service
+public class Kodawari
+		extends ApiBase<KodawariRequest, KodawariResponse> {
+
+	@Override
+	protected Class<KodawariResponse> type() {
+
+		return KodawariResponse.class;
+	}
+
+	@Override
+	protected URI uri() {
+
+		return config.getAbroad().getKodawari();
+	}
+
+}
