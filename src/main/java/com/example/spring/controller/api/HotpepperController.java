08e426167f900a06b3abb7e0828acf02cdf21fca
朝日奈ありす <59528169+asahinadev@users.noreply.github.com>
2020-07-08 13:42:23 +0900

    ホットペッパー API

--- src/main/java/com/example/spring/controller/api/HotpepperController.java
+++ src/main/java/com/example/spring/controller/api/HotpepperController.java
@@ -1,60 +1,124 @@
 package com.example.spring.controller.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.MediaType;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
-import com.example.spring.config.RecruitApiConfig;
-import com.example.spring.webapi.hotpepper.v1.api.LargeServiceArea;
-import com.example.spring.webapi.hotpepper.v1.api.ServiceArea;
-import com.example.spring.webapi.hotpepper.v1.request.LargeServiceAreaRequest;
-import com.example.spring.webapi.hotpepper.v1.request.ServiceAreaRequest;
-import com.example.spring.webapi.hotpepper.v1.responce.LargeServiceAreaResponse;
-import com.example.spring.webapi.hotpepper.v1.responce.ServiceAreaResponse;
+import com.example.spring.config.ApiConfig;
+import com.example.spring.hotpepper.request.BudgetRequest;
+import com.example.spring.hotpepper.request.CreditCardRequest;
+import com.example.spring.hotpepper.request.GenreRequest;
+import com.example.spring.hotpepper.request.GourmetRequest;
+import com.example.spring.hotpepper.request.LargeAreaRequest;
+import com.example.spring.hotpepper.request.LargeServiceAreaRequest;
+import com.example.spring.hotpepper.request.MiddleAreaRequest;
+import com.example.spring.hotpepper.request.ServiceAreaRequest;
+import com.example.spring.hotpepper.request.ShopRequest;
+import com.example.spring.hotpepper.request.SmallAreaRequest;
+import com.example.spring.hotpepper.request.SpecialCategoryRequest;
+import com.example.spring.hotpepper.request.SpecialRequest;
+import com.example.spring.hotpepper.response.BudgetResponse;
+import com.example.spring.hotpepper.response.CreditCardResponse;
+import com.example.spring.hotpepper.response.GenreResponse;
+import com.example.spring.hotpepper.response.GourmetResponse;
+import com.example.spring.hotpepper.response.LargeAreaResponse;
+import com.example.spring.hotpepper.response.LargeServiceAreaResponse;
+import com.example.spring.hotpepper.response.MiddleAreaResponse;
+import com.example.spring.hotpepper.response.ServiceAreaResponse;
+import com.example.spring.hotpepper.response.ShopResponse;
+import com.example.spring.hotpepper.response.SmallAreaResponse;
+import com.example.spring.hotpepper.response.SpecialCategoryResponse;
+import com.example.spring.hotpepper.response.SpecialResponse;
 
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@Slf4j
 @RestController
 @RequestMapping("/api/hotpepper")
-public class Hotpepper {
+public class HotpepperController {
 
 	@Autowired
-	protected RecruitApiConfig config;
+	ApiConfig config;
 
 	@Autowired
-	LargeServiceArea largeServiceArea;
+	WebClient hotpepper;
 
-	@Autowired
-	ServiceArea serviceArea;
+	public <E> Mono<E> get(WebClient client,
+			String path, MultiValueMap<String, String> params,
+			Class<E> clazz) {
 
-	@PostMapping(path = "large-service-area")
-	public LargeServiceAreaResponse.Results largeServiceArea(
-			@RequestBody(required = false) LargeServiceAreaRequest request) {
-
-		request.setKey(config.getKey());
-		return largeServiceArea.accept(request).getResults();
+		return client.get()
+				.uri(b -> b.path(path).queryParams(params).build())
+				.accept(MediaType.APPLICATION_JSON,
+						MediaType.valueOf("text/javascript;charset=UTF-8"))
+				.retrieve()
+				.bodyToMono(clazz);
 	}
 
-	@PostMapping(path = "service-area")
-	public ServiceAreaResponse.Results serviceArea(
-			@RequestBody(required = false) ServiceAreaRequest request) {
-
-		request.setKey(config.getKey());
-		return serviceArea.accept(request).getResults();
+	@GetMapping("gourmet")
+	public Mono<GourmetResponse> get(GourmetRequest request) {
+		return get(hotpepper, "gourmet/v1/", request.queries(config.getKey()), GourmetResponse.class);
 	}
 
-	@ExceptionHandler(Exception.class)
-	public ResponseEntity<String> exceptionHandler(Exception exception) {
-
-		return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+	@GetMapping("shop")
+	public Mono<ShopResponse> get(ShopRequest request) {
+		return get(hotpepper, "shop/v1/", request.queries(config.getKey()), ShopResponse.class);
 	}
 
-	@ExceptionHandler(Error.class)
-	public ResponseEntity<String> exceptionHandler(Error error) {
+	@GetMapping("budget")
+	public Mono<BudgetResponse> get(BudgetRequest request) {
+		return get(hotpepper, "budget/v1/", request.queries(config.getKey()), BudgetResponse.class);
+	}
 
-		return new ResponseEntity<>(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+	@GetMapping("large_service_area")
+	public Mono<LargeServiceAreaResponse> get(LargeServiceAreaRequest request) {
+		return get(hotpepper, "large_service_area/v1/", request.queries(config.getKey()), LargeServiceAreaResponse.class);
+	}
+
+	@GetMapping("service_area")
+	public Mono<ServiceAreaResponse> get(ServiceAreaRequest request) {
+		return get(hotpepper, "service_area/v1/", request.queries(config.getKey()), ServiceAreaResponse.class);
+	}
+
+	@GetMapping("large_area")
+	public Mono<LargeAreaResponse> get(LargeAreaRequest request) {
+		return get(hotpepper, "large_area/v1/", request.queries(config.getKey()), LargeAreaResponse.class);
+	}
+
+	@GetMapping("middle_area")
+	public Mono<MiddleAreaResponse> get(MiddleAreaRequest request) {
+		return get(hotpepper, "middle_area/v1/", request.queries(config.getKey()), MiddleAreaResponse.class);
+	}
+
+	@GetMapping("small_area")
+	public Mono<SmallAreaResponse> get(SmallAreaRequest request) {
+		return get(hotpepper, "small_area/v1/", request.queries(config.getKey()), SmallAreaResponse.class);
+	}
+
+	@GetMapping("genre")
+	public Mono<GenreResponse> get(GenreRequest request) {
+		return get(hotpepper, "genre/v1/", request.queries(config.getKey()), GenreResponse.class);
+	}
+
+	@GetMapping("credit_card")
+	public Mono<CreditCardResponse> get(CreditCardRequest request) {
+		return get(hotpepper, "credit_card/v1/", request.queries(config.getKey()), CreditCardResponse.class);
+	}
+
+	@GetMapping("special")
+	public Mono<SpecialResponse> get(SpecialRequest request) {
+		return get(hotpepper, "special/v1/", request.queries(config.getKey()), SpecialResponse.class);
+	}
+
+	@GetMapping("special_category")
+	public Mono<SpecialCategoryResponse> get(SpecialCategoryRequest request) {
+		log.debug("request => {}", request);
+		return get(hotpepper, "special_category/v1/", request.queries(config.getKey()), SpecialCategoryResponse.class);
+
 	}
 }
