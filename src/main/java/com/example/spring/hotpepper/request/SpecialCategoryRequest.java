6ddc5d3e824a2812d8f2b09f6e6927a28299eb1a
朝日奈ありす <59528169+asahinadev@users.noreply.github.com>
2020-07-08 11:45:46 +0900

    [12] 特集カテゴリマスタAPI

--- src/main/java/com/example/spring/hotpepper/request/SpecialCategoryRequest.java
+++ src/main/java/com/example/spring/hotpepper/request/SpecialCategoryRequest.java
@@ -2,33 +2,20 @@ package com.example.spring.hotpepper.request;
 
 import java.util.List;
 
-import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
 
-import lombok.Data;
+@Getter
+@Setter
+public class SpecialCategoryRequest extends CommonRequest {
 
-@Data
-public class SpecialCategoryRequest {
-
-	@JsonProperty("special_category")
-	List<String> scategory;
+	List<String> special_category;
 
 	public MultiValueMap<String, String> queries(String apiKey) {
-
-		var map = new LinkedMultiValueMap<String, String>();
-
-		// ‹¤’Ê
-		map.set("key", apiKey);
-		map.set("format", "json");
-
-		// ŒÅ—L
-		if (scategory != null && !scategory.isEmpty()) {
-			map.set("special_category", String.join(",", scategory));
-		}
-
-		return map;
-
+		var queries = super.queries(apiKey);
+		super.set(queries, "special_category", special_category);
+		return queries;
 	}
 }
