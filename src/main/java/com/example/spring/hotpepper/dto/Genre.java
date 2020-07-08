e590389ab81fbf71ccf7e45b0bd8cf86c1fa2197
朝日奈ありす <59528169+asahinadev@users.noreply.github.com>
2020-07-08 12:15:57 +0900

    null を表示しない

--- src/main/java/com/example/spring/hotpepper/dto/Genre.java
+++ src/main/java/com/example/spring/hotpepper/dto/Genre.java
@@ -1,8 +1,11 @@
 package com.example.spring.hotpepper.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.Data;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class Genre {
 	String name;
 	String code;
--- src/main/java/com/example/spring/hotpepper/dto/SpecialCategory.java
+++ src/main/java/com/example/spring/hotpepper/dto/SpecialCategory.java
@@ -1,8 +1,11 @@
 package com.example.spring.hotpepper.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.Data;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class SpecialCategory {
 	String name;
 	String code;
